bf9b6e1841a4a1f829dba6aed8ea72090a059321
Martin Dimitrov <martin.dimi97@gmail.com>
2024-11-15 21:12:16 +0000

    Switch to fugitive

--- Readme.md
+++ Readme.md
@@ -3,15 +3,21 @@
 > Current setup that i'm running + my nvim trial period.
 > Don't talk to me if this doesn't work 🙏
 
-# TODOs
+
+# Prerequisits
+* Nvim `brew install nvim`
+* Tmux `brea install tmux`
+
+# Features
+* [X] Full fledged Nvim support
 * [X] Avante support (similar to Cursor AI)
 * [X] Add markdown support. 
-* [ ] Setup tmux
+
+# Cancelled
+* [-] Setup tmux
 
 ----
 
 # Potential changes
-* [ ] Fuck iterm2, try alternatives like kitty or later on ghost
-* [ ] Potentially try fish
-* [ ] Add alternative color scheme to 
-> for colorscheme i use a [custom gruvbox material](https://github.com/adibhanna/nvim/blob/main/lua/plugins/colorschemes.lua), and for font I use https://berkeleygraphics.com/typefaces/berkeley-mono/ (or Check out SFMono it’s also really good)
+* Fuck iterm2, try alternatives like kitty or later on ghost
+* Potentially try fish
--- nvim/lua/fridge/plugins/git.lua
+++ nvim/lua/fridge/plugins/git.lua
@@ -1,20 +1,38 @@
 return {
-  "kdheepak/lazygit.nvim",
-  lazy = true,
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
-  },
-  -- optional for floating window border decoration
+  "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    "nvim-lua/plenary.nvim",  -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua",              -- optional
+    "echasnovski/mini.pick",         -- optional
   },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-  }
+  config = function()
+    local neogit = require("neogit")
+    vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    neogit.setup({})
+  end,
 }
+
+-- return {
+--   "kdheepak/lazygit.nvim",
+--   lazy = true,
+--   cmd = {
+--     "LazyGit",
+--     "LazyGitConfig",
+--     "LazyGitCurrentFile",
+--     "LazyGitFilter",
+--     "LazyGitFilterCurrentFile",
+--   },
+--   -- optional for floating window border decoration
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--   },
+--   -- setting the keybinding for LazyGit with 'keys' is recommended in
+--   -- order to load the plugin when the command is run for the first time
+--   keys = {
+--     { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+--   }
+-- }
