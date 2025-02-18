b63d6a313271a79b9265f4e142de99c78c82ab4b
Martin Dimitrov <martin@Martins-MacBook-Pro.local>
2025-02-18 10:37:32 +0000

    add userled

---
+++ zsh/userled.sh
@@ -0,0 +1,12 @@
+export USERLED_HOME=$HOME/Documents/core
+
+[ -f $ZDOTDIR/gcloud.sh ] && source $ZDOTDIR/gcloud.sh
+
+# TODO: MOve elsewhere
+alias core="cd $USERLED_HOME"
+alias be="cd $USERLED_HOME/backend"
+alias fe="cd $USERLED_HOME/frontend"
+alias sdk="cd $USERLED_HOME/sdk"
+alias ldb="bash $USERLED_HOME/experimental/martin/db/db-local.sh"
+alias sdb="bash $USERLED_HOME/experimental/martin/db/db.sh"
+alias pdb="bash $HOME/Documents/scripts/userled_prod.sh"
