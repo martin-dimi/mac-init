09ac8649e54a737bcc2e12f1fa585ddd6aeef95c
Martin Dimitrov <martin.dimi97@gmail.com>
2025-04-04 22:36:32 +0100

    Update git commits

--- nvim/lua/fridge/plugins/git.lua
+++ nvim/lua/fridge/plugins/git.lua
@@ -18,15 +18,19 @@ return {
   },
   {
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+      })
+
+      vim.keymap.set('n', '<leader>gp', "<cmd>Gitsigns preview_hunk<CR>", { desc = "Preview" })
+    end,
   },
   {
     "FabijanZulj/blame.nvim",
