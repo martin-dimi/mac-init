24346f3265f90f56c0a96c5fc52e561b0a6de916
Martin Dimitrov <martin.dimi97@gmail.com>
2025-09-10 23:56:45 +0200

    Remove old themes

--- nvim/lua/fridge/plugins/theme/colorscheme.lua
+++ nvim/lua/fridge/plugins/theme/colorscheme.lua
@@ -6,56 +6,6 @@ return {
 
 		dependencies = {
 			{
-				"catppuccin/nvim",
-				name = "catppuccin",
-				lazy = true,
-				opts = {
-					integrations = {
-						blink_cmp = true,
-						gitsigns = true,
-						treesitter = true,
-						notify = false,
-						copilot_vim = true,
-						snacks = {
-							enabled = false,
-							indent_scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
-						},
-						telescope = {
-							enabled = true,
-							-- style = "nvchad"
-						},
-						lsp_trouble = true,
-						which_key = true,
-						nvim_surround = true,
-						render_markdown = true,
-						native_lsp = {
-							enabled = true,
-							virtual_text = {
-								errors = { "italic" },
-								hints = { "italic" },
-								warnings = { "italic" },
-								information = { "italic" },
-								ok = { "italic" },
-							},
-							underlines = {
-								errors = { "underline" },
-								hints = { "underline" },
-								warnings = { "underline" },
-								information = { "underline" },
-								ok = { "underline" },
-							},
-							inlay_hints = {
-								background = true,
-							},
-						},
-						-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-					},
-				},
-				-- config = function()
-				--   SetupCatppuccin()
-				-- end
-			},
-			{
 				"rebelot/kanagawa.nvim",
 				lazy = false,
 				priority = 1000,
